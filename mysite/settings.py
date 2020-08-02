4fc6f1ad0491fc6b036435abf95951b51f2bc697
Grzegorz-Giedrojc <66877459+Grzegorz-Giedrojc@users.noreply.github.com>
2020-08-02 15:28:07 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5)u1lk3g1!qr7rwtcjbo7u449e)%^sm&a3b8(s(6zn(rpex%+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
